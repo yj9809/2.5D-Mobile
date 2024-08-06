52abd45f99cc409b61643eb59ca16a7e7e474480
Kw98 <147613533+Kw98@users.noreply.github.com>
2024-08-06 12:07:12 +0900

    + ConveyerBelt Scirpt
    컨베이어 벨트 스크립트 수정

--- Assets/1. Scripts/Churu/ConveyerBelt.cs
+++ Assets/1. Scripts/Churu/ConveyerBelt.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class ConveyerBelt : MonoBehaviour
 {
-    [SerializeField] private Transform nextBelt;
     [SerializeField] private float speed; // 3
-    [SerializeField] private Vector3 moveBelt = Vector3.left; // -1
-    [SerializeField] private float beltSize; // 2.3
+    [SerializeField] private Vector3 dir; // Z : -1
+    [SerializeField] private List<GameObject> onBelt;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += moveBelt * speed * Time.deltaTime;
-
-        if (transform.position.x <= -beltSize)
+        for (int i = 0; i <= onBelt.Count - 1; i++)
         {
-            transform.position = nextBelt.position + Vector3.right * beltSize;
+            onBelt[i].GetComponent<Rigidbody>().velocity = speed * dir;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onBelt.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onBelt.Remove(collision.gameObject);
     }
 }
