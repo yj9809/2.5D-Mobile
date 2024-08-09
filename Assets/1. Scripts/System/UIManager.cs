723401ade9a846beebde03f6dfc5f4cfee40b23f
Kw98 <147613533+Kw98@users.noreply.github.com>
2024-08-09 12:23:44 +0900

    +Gold System
    UIManager스크립트를 통해 Car에 물품을 옮겨 재화를 획득하는 시스템 구축

--- Assets/1. Scripts/System/UIManager.cs
+++ Assets/1. Scripts/System/UIManager.cs
@@ -5,12 +5,11 @@ using UnityEngine.UI;
 using TMPro;
 using Sirenix.OdinInspector;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private TextMeshProUGUI goldTxt;
 
     private int gold = 0;
-    private int itemPrice = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -34,7 +33,31 @@ public class UIManager : MonoBehaviour
 
     public void SellItem()
     {
-        gold += itemPrice;
-        UpdateUI();
+        //Å×½ºÆ®¿ë
+        AddGold(10);
     }
+
+    public void AddGold(int amount)
+    {
+        gold += amount;
+        UpdateUI();
+        Debug.Log($"°ñµå È¹µæ: {amount}, ÇöÀç °ñµå: {gold}");
+    }
+
+    //°ñµå »ç¿ë ÇÔ¼ö
+    //public bool SpendGold(int amount)
+    //{
+    //    if (gold >= amount)
+    //    {
+    //        gold -= amount;
+    //        UpdateUI();
+    //        Debug.Log($"°ñµå »ç¿ë: {amount}, ³²Àº °ñµå: {gold}");
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("°ñµå°¡ ºÎÁ·ÇÕ´Ï´Ù !");
+    //        return false;
+    //    }
+    //}
 }
