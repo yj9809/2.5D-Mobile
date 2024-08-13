792a12a59b36ec2dff79a721fa62654310688059
Kw98 <147613533+Kw98@users.noreply.github.com>
2024-08-13 12:10:09 +0900

    +Sound System
    임시 배경음악 추가 및 소리기능 On/Off 추가

--- Assets/1. Scripts/System/Option.cs
+++ Assets/1. Scripts/System/Option.cs
@@ -2,22 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Option : MonoBehaviour
 {
-    [SerializeField] private Button optionButton;
+    // 사운드 추가 후 다시 테스트 해야함
     [SerializeField] private GameObject blurPanel;
+    [SerializeField] private Button soundToggleButton;
+    [SerializeField] private AudioSource audioSource;
+    private bool isSoundOn = true;
+    private MainCamera camera0;
 
     // Start is called before the first frame update
     void Start()
     {
+        camera0 = FindObjectOfType<MainCamera>();
+
         blurPanel.SetActive(false);
+
+        isSoundOn = PlayerPrefs.GetInt("SoundState", 1) == 1;
+        SoundText();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SoundButton()
     {
-        
+        isSoundOn = !isSoundOn;
+        audioSource.mute = !isSoundOn;
+
+        PlayerPrefs.SetInt("SoundState", isSoundOn ? 1 : 0);
+        PlayerPrefs.Save();
+
+        SoundText();
+    }
+
+    private void SoundText()
+    {
+        soundToggleButton.GetComponentInChildren<TextMeshProUGUI>().text = isSoundOn ? "Sound ON" : "Sound OFF";
+    }
+
+    public void Zoom()
+    {
+        camera0.ZoomScreen();
     }
 
     public void ShowOption()
