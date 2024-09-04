5336204437b5084075feb09e418fd13061418f83
Kw98 <147613533+Kw98@users.noreply.github.com>
2024-09-04 12:32:35 +0900

    튜토리얼 가이드라인 추가

---
+++ Assets/1. Scripts/System/TutorialGuide.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialGuide : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform target;
+    [SerializeField] private RectTransform canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameManager.Instance.P.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GuideLine();
+    }
+
+    private void GuideLine()
+    {
+        Vector3 direction = target.position - player.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        canvas.rotation = lookRotation * Quaternion.Euler(90, 0, 0);
+
+        canvas.localPosition = Vector3.zero;
+    }
+}
