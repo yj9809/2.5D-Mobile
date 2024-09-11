9c134080d8f920f0e5e274254fbe7ddb9037ac33
yj9809 <147613573+yj9809@users.noreply.github.com>
2024-09-11 14:01:02 +0900

    회원가입

---
+++ Assets/1. Scripts/TestLogin.cs
@@ -0,0 +1,76 @@
+using BackEnd;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TestLogin : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField id;
+    [SerializeField] private TMP_InputField pw;
+
+    private void Start()
+    {
+        var bro = Backend.Initialize(); // 뒤끝 초기화
+
+        // 뒤끝 초기화에 대한 응답값
+        if (bro.IsSuccess())
+        {
+            Debug.Log("초기화 성공 : " + bro); // 성공일 경우 statusCode 204 Success
+        }
+        else
+        {
+            Debug.LogError("초기화 실패 : " + bro); // 실패일 경우 statusCode 400대 에러 발생
+        }
+    }
+
+    public void Test()
+    {
+        BackendLogin.Instance.CustomSignUp(id.text, pw.text);
+        Debug.Log("테스트를 종료합니다.");
+    }
+}
+public class BackendLogin
+{
+    private static BackendLogin _instance = null;
+
+    public static BackendLogin Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new BackendLogin();
+            }
+
+            return _instance;
+        }
+    }
+
+    public void CustomSignUp(string id, string pw)
+    {
+        Debug.Log("회원가입을 요청합니다.");
+
+        var bro = Backend.BMember.CustomSignUp(id, pw);
+
+        if (bro.IsSuccess())
+        {
+            Debug.Log("회원가입에 성공했습니다. : " + bro);
+        }
+        else
+        {
+            Debug.LogError("회원가입에 실패했습니다. : " + bro);
+        }
+    }
+
+    public void CustomLogin(string id, string pw)
+    {
+        // Step 3. 로그인 구현하기 로직
+    }
+
+    public void UpdateNickname(string nickname)
+    {
+        // Step 4. 닉네임 변경 구현하기 로직
+    }
+}
