6029b822dedfb2f58aa126f2d1770edb0a8d3ecc
yj9809 <147613573+yj9809@users.noreply.github.com>
2024-11-06 19:47:46 +0900

    마을 추가

--- Assets/1. Scripts/Work/TruckDoor.cs
+++ Assets/1. Scripts/Work/TruckDoor.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class TruckDoor : MonoBehaviour
 {
     [SerializeField] private Truck truck;
+    [SerializeField] private Collider collider;
+    [SerializeField] private Animator door;
 
     public void DoorOpen()
     {
-        truck.DoorOpen();
+        if(truck != null)
+            truck.DoorOpen();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(collider != null && other.GetComponent<Player>())
+        {
+            door.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (collider != null && other.GetComponent<Player>())
+        {
+            door.SetBool("Open", false);
+        }
     }
 }
